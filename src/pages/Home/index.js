b4261fb2ed5800e4e1b717e5296171a35aa475a3
Fagner <fagner.ufpa@gmail.com>
2022-02-03 00:18:36 -0300

    Modal

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,19 @@
 
+import { useState } from 'react'
 import { FiLink } from 'react-icons/fi';
 import './home.css';
 
 import Menu from '../../components/Menu';
+import LinkItem from '../../components/LinkItem';
 function Home() {
+
+    const [link, setLink] = useState('');
+    const [showModal, setShowModal] = useState(false);
+
+    function handleShortLInk() {
+        setShowModal(true)
+    }
+
     return (
         <div className="container-home">
             <div className="logo">
@@ -19,15 +29,19 @@ function Home() {
                         type="text"
                         placeholder="Cole seu link aqui..."
                         name="link"
+                        value={link}
+                        onChange={(e) => { setLink(e.target.value) }}
                     />
                 </div>
 
-                <button>Gerar Link</button>
+                <button onClick={handleShortLInk}>Gerar Link</button>
 
 
             </div>
 
             <Menu></Menu>
+
+            {showModal && (<LinkItem closeModal={() => setShowModal(false)} />)}
         </div>
 
 
