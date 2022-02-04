c0a8e906add847890e496bb76cf26724b74cf4c3
Fagner <fagner.ufpa@gmail.com>
2022-02-04 19:10:28 -0300

    Finalizado Correção

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,7 +7,7 @@ import Menu from '../../components/Menu';
 import LinkItem from '../../components/LinkItem';
 
 import api from '../../services/api';
-import { saveLink, getLinks } from '../../services/storeLinks';
+import { saveLink } from '../../services/storeLinks';
 function Home() {
 
     const [link, setLink] = useState('');
